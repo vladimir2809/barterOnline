3959c0d934c87c367c2f1b988c7b98144146aeab
Vovchik-Ruchkin <vovcikruckin@gmail.com>
2025-05-07 17:40:00 +0500

    свертал форму новый бартер

--- views/script/script.js
+++ views/script/script.js
@@ -7,4 +7,23 @@ window.addEventListener('load',()=>{
     resultNode.append(cloneResultItem);
     cloneResultItem=resultItemNode.cloneNode(true);
     resultNode.append(cloneResultItem);
+    // previewFile();
 });
+setInterval(previewFile,1000);
+function previewFile() {
+    var preview = document.getElementById('img-give-preload');
+    var file    = document.querySelector('input[type=file]').files[0];
+    var reader  = new FileReader();
+  
+    reader.onloadend = function () {
+      preview.src = reader.result;
+      console.log(589);
+    }
+  
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = "";
+    }
+    console.log(file);
+  }
