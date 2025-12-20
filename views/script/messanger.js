16d9abd8dfa147d9e586ea4fe9042cea8d72933f
Владимир Костенко <kost.vova@mail.ru>
2025-12-20 16:25:25 +0500

    Сделал кнопку назад у мессанджера в мобайл

--- views/script/messanger.js
+++ views/script/messanger.js
@@ -1,12 +1,13 @@
 var widthScreenValue=430;
 var widthOnlyCantacts=500;
-var widthButtonSend=45;
+var widthButtonSend=40;
 var flagNoContactView=false;
 var widthScreen=window.innerWidth;
 
 var contacts = document.getElementsByClassName('contact');
 var contactSelect = document.getElementsByClassName('contact-select')[0];
 var textBlock = document.getElementsByClassName('text-block')[0];
+var arrowBack=document.getElementsByClassName('text-block__info-back')[0];
 
 // alert('AIM MESSANGER JS');
 var sendInput=document.getElementById('send-input');
@@ -26,23 +27,26 @@ setInterval(function(){
     widthScreen=window.innerWidth;
     let heightScreen=window.innerHeight;
     console.log(widthScreen);
+    if (widthScreen < widthOnlyCantacts)
+    {
+        arrowBack.style.display='block';
+    }
+    else 
+    {
+        arrowBack.style.display='none';
+    }
     if (widthScreen > widthScreenValue)
     {
         if(flagNoContactView==false)
         {   
-            let widthInput=widthScreen-(widthScreenValue-widthButtonSend)
-            //widthInput/=10;
-            // let heightInput=heightScreen-
+            let widthInput=widthScreen-(widthScreenValue-widthButtonSend*1.25/*0.875*/)
             sendInput.style.width=`${widthInput}px`;
         }
     }
-    //if (widthScreen < widthOnlyCantacts)
     {
         if(flagNoContactView==true)
         {   
-            let widthInput=widthScreen-widthButtonSend*1.6;
-            //widthInput/=10;
-            // let heightInput=heightScreen-
+            let widthInput=widthScreen-widthButtonSend*1.8//2.2;
             sendInput.style.width=`${widthInput}px`;
         }
     }
@@ -60,15 +64,20 @@ for (let i=0; i < contacts.length;i++)
             textBlock.style.display="block";
             textBlock.style.gridColumn="1 / 3";
             textBlock.style.width='100%';
-            let widthInput=widthScreen//-(widthScreenValue-45)
-            //widthInput/=10;
-            // let heightInput=heightScreen-
-            sendInput.style.width=`${widthInput}px`;
         }
     })
     
 }
-
+arrowBack.addEventListener('click', function(event){
+    //if (widthScreen < widthOnlyCantacts)
+    {
+        flagNoContactView=false;
+        contactSelect.style.display='block';
+        textBlock.style.display="none";
+        textBlock.style.gridColumn="2 / 3";
+        // textBlock.style.width='100%';
+    }
+})
 function getRandomColor() {
   // Генерация случайного числа и преобразование в HEX-строку
   const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padEnd(6, '0');
