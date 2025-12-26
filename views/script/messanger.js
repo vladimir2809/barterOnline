d4f9d84459f8975f91033b5ede9cec26e1a8d6a9
Владимир Костенко <kost.vova@mail.ru>
2025-12-26 13:48:24 +0500

    Настроил прокрутку у мессанджера

--- views/script/messanger.js
+++ views/script/messanger.js
@@ -1,4 +1,4 @@
-var widthScreenValue=430;
+var widthScreenValue=500//430;
 var widthOnlyCantacts=500;
 var widthButtonSend=40;
 var flagNoContactView=false;
@@ -30,23 +30,48 @@ setInterval(function(){
     if (widthScreen < widthOnlyCantacts)
     {
         arrowBack.style.display='block';
+        if (flagNoContactView==false)
+        {
+            contactSelect.style.display='block';
+            textBlock.style.display="none";
+            textBlock.style.gridColumn="1 / 3";
+        }
     }
-    else 
+    if (flagNoContactView==true && widthScreen < widthOnlyCantacts)
+    {
+        goPressContactsOnly()
+        // flagNoContactView=true;
+        // flagNoContactView=false;
+        // contactSelect.style.display='none';
+        // textBlock.style.display="block";
+        // textBlock.style.gridColumn="1 / 3";
+    }
+    if (widthScreen >= widthOnlyCantacts) 
     {
         arrowBack.style.display='none';
+        // goPressButtonBack()
+        // flagNoContactView=true;
+        // if (flagNoContactView==false)
+        {
+            // flagNoContactView=true;
+            contactSelect.style.display='block';
+            textBlock.style.display="block";
+            textBlock.style.gridColumn="2 / 3";
+        }
     }
+   
     if (widthScreen > widthScreenValue)
     {
         if(flagNoContactView==false)
         {   
-            let widthInput=widthScreen-(widthScreenValue-widthButtonSend*1.25/*0.875*/)
+            let widthInput=widthScreen-(widthScreenValue-widthButtonSend*0.25/*0.875*/)
             sendInput.style.width=`${widthInput}px`;
         }
     }
     {
         if(flagNoContactView==true)
         {   
-            let widthInput=widthScreen-widthButtonSend*1.8//2.2;
+            let widthInput=widthScreen-widthButtonSend *.8//2.2;
             sendInput.style.width=`${widthInput}px`;
         }
     }
@@ -59,11 +84,14 @@ for (let i=0; i < contacts.length;i++)
     contacts[i].addEventListener("click", function(event){
         if (widthScreen < widthOnlyCantacts)
         {
+
             flagNoContactView=true;
-            contactSelect.style.display='none';
-            textBlock.style.display="block";
-            textBlock.style.gridColumn="1 / 3";
-            textBlock.style.width='100%';
+            goPressContactsOnly()
+            // flagNoContactView=true;
+            // contactSelect.style.display='none';
+            // textBlock.style.display="block";
+            // textBlock.style.gridColumn="1 / 3";
+            // textBlock.style.width='100%';
         }
     })
     
@@ -71,13 +99,30 @@ for (let i=0; i < contacts.length;i++)
 arrowBack.addEventListener('click', function(event){
     //if (widthScreen < widthOnlyCantacts)
     {
-        flagNoContactView=false;
-        contactSelect.style.display='block';
-        textBlock.style.display="none";
-        textBlock.style.gridColumn="2 / 3";
+        goPressButtonBack()
+
+        // flagNoContactView=false;
+        // contactSelect.style.display='block';
+        // textBlock.style.display="none";
+        // textBlock.style.gridColumn="2 / 3";
         // textBlock.style.width='100%';
     }
 })
+function goPressButtonBack()// когда нажимаю на стрелочку назад
+{
+    flagNoContactView=false;
+    contactSelect.style.display='block';
+    textBlock.style.display="none";
+    textBlock.style.gridColumn="2 / 3";
+}
+function goPressContactsOnly() // когда кликаю на контакт в малом разрешении
+{
+    // flagNoContactView=true;
+    contactSelect.style.display='none';
+    textBlock.style.display="block";
+    textBlock.style.gridColumn="1 / 3";
+    textBlock.style.width='100%';
+}
 function getRandomColor() {
   // Генерация случайного числа и преобразование в HEX-строку
   const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16).padEnd(6, '0');
